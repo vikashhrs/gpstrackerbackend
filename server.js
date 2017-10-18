a631ed8bfd4871d3259a549a8ac23c14e10b35fe
Vikash Kumar Sharma <vikashhrs@gmail.com>
2017-10-18 12:56:51 +0530

    kicking off

--- server.js
+++ server.js
@@ -3,6 +3,9 @@ var PORT = process.env.port | 3000;
 
 var app = express();
 
+app.get('/',function(req,res){
+    res.status(200).send("<h1>Watching you</h1>")
+})
 
 app.listen(PORT,function(){
     console.log("Server running on port "+PORT);
